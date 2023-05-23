126f1ecc3633db7cc7cc595ef35133ac00d587d7
ArnoMasian <hnaisam@gmail.com>
2023-05-23 11:39:35 +0500

    all employees fix 6

--- app/api/employees/route.js
+++ app/api/employees/route.js
@@ -5,12 +5,10 @@ export const GET = async (request) => {
   try {
     await connectToDB();
 
-    const employees = await Employee.find({})
-      .populate("creator")
-      .populate({
-        path: "requests",
-        match: { _id: { $exists: true } },
-      });
+    const employees = await Employee.find({}).populate([
+      { path: "creator" },
+      { path: "requests" },
+    ]);
 
     return new Response(JSON.stringify(employees), { status: 200 });
   } catch (error) {

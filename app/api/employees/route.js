fb3c5c08e52b390f21d699fba569561f1c593135
ArnoMasian <hnaisam@gmail.com>
2023-05-23 11:36:56 +0500

    all employees fix 5

--- app/api/employees/route.js
+++ app/api/employees/route.js
@@ -4,8 +4,13 @@ import Employee from "@models/employee";
 export const GET = async (request) => {
   try {
     await connectToDB();
-    const employees = await Employee.find({});
-    await Employee.populate(employees, { path: "requests" });
+
+    const employees = await Employee.find({})
+      .populate("creator")
+      .populate({
+        path: "requests",
+        match: { _id: { $exists: true } },
+      });
 
     return new Response(JSON.stringify(employees), { status: 200 });
   } catch (error) {
